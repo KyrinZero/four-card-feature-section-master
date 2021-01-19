681eadf5495824cfcbeb58b36a2d3f7854a0bd48
Bayajit Mohammed <bayajitmohammed@gmail.com>
2021-01-20 00:55:36 +0800

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -57,7 +57,11 @@ export default function App() {
       <footer>
         <p className="attribution">
           Challenge by{" "}
-          <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">
+          <a
+            href="https://www.frontendmentor.io?ref=challenge"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
             Frontend Mentor
           </a>
           . Coded by{" "}
